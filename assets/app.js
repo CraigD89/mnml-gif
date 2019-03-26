0560d952c147f7e69dc50ec125b8d994c497f0cf
Craig D <dupuiscraig@gmail.com>
2019-03-26 12:36:09 -0500

    adds functionality to user input and search button

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,26 @@ var userInput = document.querySelector("#userInput");
 var button = document.querySelector("#searchBtn");
 var gifResult = document.querySelector("#gifResult");
 
+// Stores value of user's input
+
+
 // console.log(userInput);
 // console.log(button);
 // console.log(gifResult);
+
+// Takes in user's input
+userInput.addEventListener("keydown", function(value) {
+ 
+ var searchInput = document.querySelector("#userInput").value;
+
+ 
+    if (value.key === "Enter") {
+    console.log(searchInput);
+  }
+
+});
+
+// Adds functionality to the search button
+button.addEventListener("click", function() {
+  console.log("Button clicked");
+});
